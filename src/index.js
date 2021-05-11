bf7d8342b25f6f29ef56b633126c641428de1793
Dmytro Anikin <dimianni@users.noreply.github.com>
2021-05-11 21:15:43 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import _ from 'lodash'
+// import html from './index.html'
+import { test } from './test.js'
+import './scss/style.scss'
+import './images/dimianni_logo.svg'
+
+console.log(_.join(['Hey', 'world', '!!!'], ' '));
+
+test()
---
+++ src/test.js
@@ -0,0 +1,10 @@
+// import _ from 'lodash'
+import './test.css'
+
+function test() {
+    // console.log(_.join(['Teeeeeestttttt', 'meeee', '!'], ' '));
+    console.log('Letsssgogooo');
+}
+
+// https://medium.com/backticks-tildes/introduction-to-es6-modules-49956f580da
+export { test }
